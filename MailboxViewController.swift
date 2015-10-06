804691b577cccba1906db50cf9cc1870027cb6f9
Prisca Ekkens <pekkens@mac.com>
2015-10-05 21:57:23 -0700

    updated

--- MailboxViewController.swift
+++ MailboxViewController.swift
@@ -8,28 +8,81 @@
 
 import UIKit
 
-class MailboxViewController: UIViewController {
-
+class MailboxViewController: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet weak var feedScrollView: UIScrollView!
+    @IBOutlet weak var messageView: UIView!
+    @IBOutlet weak var iconScreenView: UIImageView!
+    @IBOutlet var iconScreenTapGesture: UITapGestureRecognizer!
+    @IBOutlet weak var listView: UIView!
+    
+    //MARK: Variables
+    
+    var messageViewOriginalCenter: CGPoint!
+    var originalPosition: CGFloat!
+    
+    var savePosition:CGFloat!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        messageViewOriginalCenter = messageView.center
+        
+        feedScrollView.delegate = self
+        feedScrollView.contentSize = CGSize(width: 320, height: 1200)
+        
+        iconScreenView.alpha = 0
+        
+        print(messageViewOriginalCenter)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //MARK: Actions
+    
+    @IBAction func onMessageViewPanGesture(sender: UIPanGestureRecognizer) {
+        
+        let location = sender.locationInView(view)
+        let translation = sender.translationInView(view)
+        var laterPosition: CGFloat!
+        let deletePosition: CGFloat!
+        let offPosition: CGFloat!
+        
+        laterPosition = 100
+        offPosition = -300
+        
+        if sender.state == UIGestureRecognizerState.Began {
+            messageViewOriginalCenter.x = messageView.center.x
+            
+            
+            
+            print("Gesture began")
+            
+        }
+            
+        else if sender.state == UIGestureRecognizerState.Changed {
+            messageView.center.x = CGFloat(messageViewOriginalCenter.x + translation.x)
+            
+            print (translation.x)
+            print("Gesture changed")
+            
+        }
+            
+        else if sender.state == UIGestureRecognizerState.Ended {
+            print("Gesture ended")
+            
+            if location.x > -5 {
+                self.listView.backgroundColor = UIColor.isYellowColor()
+                self.messageView.center.x = laterPosition
+            }
+        
+            else if location.x > laterPosition {
+            self.listView.backgroundColor = UIColor.isBrownColor()
+            self.messageView.center.x = offPosition
+            
+            iconScreenView.alpha = 1
+                
+            }
+        }
     }
-    */
-
 }
+
